0216081e404abfbd9657f39f2a9417d3f9986f2f
Nirbhay97 <83468600+Nirbhay97@users.noreply.github.com>
2023-04-05 19:55:49 +0530

    styled

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import './App.css';
+import Navbar from './Components/Navbar/Navbar';
+import Home from './Components/Home/Home';
+import Footer from './Components/Footer/Footer';
 
 function App() {
   return (
-    <div className="App">
-       React app he thre 
-    </div>
-  );
+    <div className='App'>
+      <Navbar/>
+      <Home/>
+      <Footer/>
+    </div>   
+  )
 }
 
 export default App;
+
+
+//wesites - resources
+
+//https://nirbhayresume.onrender.com/
+//https://pixabay.com/videos/stars-long-exposure-starry-sky-6962/
---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+const Footer = () =>{
+    return (
+        <section className="glass-effect WorkExperience">
+            <div className="aboutDiv">
+                I am tehr to help you gys,
+                ttttttttttttttttttttttttt
+                tttttttttttttttttttttttttttt
+                ttttttttttttt
+                has been the industry's standard dummy text ever since the 1500s, 
+            when an unknown printer took a galley of type and scrambled it to mak
+            e a type specimen book. It has survived not only five centuries, but a
+            lso the leap into electronic typesetting, remaining essentially un
+            changed. It was popularised in the 1960s with the release of L
+            etraset sheets containing Lorem Ipsum passages, and more recentl
+            y with desktop publishing software like Aldus PageMaker including 
+             will uncover many web sites still in their
+             Hi there
+            Lorem Ipsum is simply dummy t
+            ext of the printing and typesetting industry. Lorem Ipsum 
+            has been the industry's standard dummy text ever since the 1500s, 
+            when an unknown printer took a galley of type and scrambled it to mak
+            e a type specimen book. It has survived not only five centuries, but a
+            lso the leap into electronic typesetting, remaining essentially un
+            changed. It was popularised in the 1960s with the release of L
+            etraset sheets containing Lorem Ipsum passages, and more recentl
+            y with desktop publishing software like Aldus PageMaker including 
+             will uncover many web sites still in their
+            </div>
+        </section>
+    )
+}
+
+export default Footer
