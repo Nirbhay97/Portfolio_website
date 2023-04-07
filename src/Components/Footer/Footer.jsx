aecc9d875f6b58bec72d1e5a02692092ccb6dc42
Nirbhay97 <83468600+Nirbhay97@users.noreply.github.com>
2023-04-08 01:30:48 +0530

    mobile view fixed

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,33 +1,15 @@
 import React from "react";
+import "./Footer.css";
 
 const Footer = () =>{
     return (
-        <section className="glass-effect WorkExperience">
-            <div className="aboutDiv">
-                I am tehr to help you gys,
-                ttttttttttttttttttttttttt
-                tttttttttttttttttttttttttttt
-                ttttttttttttt
-                has been the industry's standard dummy text ever since the 1500s, 
-            when an unknown printer took a galley of type and scrambled it to mak
-            e a type specimen book. It has survived not only five centuries, but a
-            lso the leap into electronic typesetting, remaining essentially un
-            changed. It was popularised in the 1960s with the release of L
-            etraset sheets containing Lorem Ipsum passages, and more recentl
-            y with desktop publishing software like Aldus PageMaker including 
-             will uncover many web sites still in their
-             Hi there
-            Lorem Ipsum is simply dummy t
-            ext of the printing and typesetting industry. Lorem Ipsum 
-            has been the industry's standard dummy text ever since the 1500s, 
-            when an unknown printer took a galley of type and scrambled it to mak
-            e a type specimen book. It has survived not only five centuries, but a
-            lso the leap into electronic typesetting, remaining essentially un
-            changed. It was popularised in the 1960s with the release of L
-            etraset sheets containing Lorem Ipsum passages, and more recentl
-            y with desktop publishing software like Aldus PageMaker including 
-             will uncover many web sites still in their
+        <section className="glass-effect Footerrr">
+            <div className="aboutDivF">
+                Made with ❤️ by Nirbhay !
             </div>
+            {/* <div className="aboutDivF">
+               Want the source code, kindly Contact !
+            </div> */}
         </section>
     )
 }
