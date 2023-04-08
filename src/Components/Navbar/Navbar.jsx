4b05f3e32e58a966bbed342e5033532573894101
Nirbhay97 <83468600+Nirbhay97@users.noreply.github.com>
2023-04-08 12:22:56 +0530

    mobile view for navbar fixed

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,8 +1,26 @@
-import React from "react";
+import React, {useState} from "react";
 import "./Navbar.css";
+import { AiFillCloseCircle } from "react-icons/ai";
+import { TfiLayoutGrid3Alt } from "react-icons/tfi";
 
 
 const Navbar = () => {
+
+  const [active, setActive] = useState("n-right");
+  const [active1, setActive1] = useState("n-list");
+  const [active3, setActive3] = useState("");
+  
+  const showNav = ()=>{
+   
+    setActive("n-right-1");
+    setActive1("n-list-1");
+    setActive3("ul-class");
+  }
+
+  const removeNav = ()=>{
+    setActive("n-right");
+    setActive1("n-list");
+  }
   return (
     <div className="n-wrapper" id="Navbar">
       {/* left */}
@@ -11,8 +29,8 @@ const Navbar = () => {
         
       </div>
       {/* right */}
-      <div className="n-right">
-        <div className="n-list">
+      <div className={active}>
+        <div className={active1}>
           <ul style={{ listStyleType: "none" }}>
             <li>
 
@@ -38,10 +56,16 @@ const Navbar = () => {
               
             </li>
           </ul>
+
         </div>
         
         <button className="button n-button">Contact</button>
-        
+        <div onClick={removeNav} className="closeNavbar">
+          <AiFillCloseCircle className="icon" />
+        </div>
+      </div>
+      <div onClick={showNav} className="toggleNavbar">
+          <TfiLayoutGrid3Alt className="icon" />
       </div>
     </div>
   );
